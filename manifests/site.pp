31ed329723e6db810336f206e7210d2f4bae4ffe
rockko <sksharma1920@gmail.com>
2023-12-06 17:55:16 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,7 +3,8 @@ node default {
     ensure => file,
     content => 'this is readme',
     owner => 'root,
+   }
  }
-node 'master.puppet.vm' {
+ node 'master.puppet.vm' {
    include role::master_server
-  }
+}
